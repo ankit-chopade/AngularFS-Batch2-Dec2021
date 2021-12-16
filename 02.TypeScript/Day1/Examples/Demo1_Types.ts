9001c75f4a2ba626c6a13637e1046f21b6299e21
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2021-12-16 16:15:58 +0530

    Delete Demo1_Types.ts

--- 02.TypeScript/Day1/Examples/Demo1_Types.ts
+++
@@ -1,8 +0,0 @@
-let uname:string  =  "Scott";
-let age:number  =  25;
-let isValidUser:boolean = true;
-
-console.log("User Name  : " + uname);
-console.log("User Age  : " + age);
-console.log("Is Valid User : " + isValidUser);
-
